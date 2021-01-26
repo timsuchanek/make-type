59cfdb7dfb589dbedab7da017721d9c97aab23c2
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-01-26 17:08:03 +0100

    improve type soundness

--- Exact.ts
+++ Exact.ts
@@ -2,10 +2,35 @@
  * `Exact` forces a type to comply by another type. It will need to be a subset
  * and must have exactly the same properties, no more, no less.
  */
-type Exact<A, W> = {
+// type Exact<A, W> = {
+//     [K in keyof W]: K extends keyof A
+//     ? A[K] extends W[K] ? A[K] & W[K] : W[K]
+//     : W[K]
+// } & {
+//     [K in keyof A]: K extends keyof W
+//     ? A[K] extends W[K] ? A[K] & W[K] : W[K]
+//     : never
+// }
+
+/**
+ * `Exact` forces a type to comply by another type. It will need to be a subset
+ * and must have exactly the same properties, no more, no less.
+ */
+type Exact<A, W> = 
+A extends W ? W extends unknown ? 
+A extends NotMappableType ? A : {
     [K in keyof A]: K extends keyof W
-    ? A[K] extends W[K] ? A[K] & W[K] : W[K]
+    ? Exact<A[K], W[K]>
     : never
-}
+} : never : W
+
+type NotMappableType =
+| string
+| number
+| bigint
+| boolean
+| ((...args: any) => any);
 
 export {Exact};
+
+type Try<A, B> = A extends B ? A : never
--- draft.ts
+++ draft.ts
@@ -1,13 +1,12 @@
 import { PrismaClient, Prisma } from '@prisma/client'
 import { Exact } from './Exact';
-import { Narrow } from './Narrow';
 
 /**
  * The ideal way of doing is the one you pointed out, of course. But we are
  * limited by the language, we would need to wait for the sigil/optional type
  * parameters proposal that is in discussion for years.
  */
-declare function validateType<V, S>(select: Narrow<S, V>): S;
+declare function validateType<V, S>(select: Exact<S, V>): S;
 
 /**
  * And this is impossible ATM because TypeScript wants all or nothing when it
@@ -15,7 +14,7 @@ declare function validateType<V, S>(select: Narrow<S, V>): S;
  * 
  * [info] https://github.com/microsoft/TypeScript/issues/14400
  */
-const test0 = validateType<Prisma.UserSelect>({ id: true });
+validateType<Prisma.UserSelect>({ id: true });
 
 /**
  * TS has a limitations, we cannot 'just' pass one of the generics. So we have
@@ -35,9 +34,48 @@ async function main() {
   const data0 = await prisma.user.findFirst({ select: select0 });
   const data1 = await prisma.user.findFirst({ select: select1 });
 
-  const select4 = userValidator({ asd: true });  // now errors
+  const select4 = userValidator({ asd: true });
 
   prisma.$disconnect();
 }
+
+const test0 = validator<{a: number}>()({ a: '2', b: 2 });
+const test1 = validator<{a: number}>()({ a: 2, b: 2 });
+const test2 = validator<{a: number}>()({ a: 1 });
+const test3 = validator<{a: number}>()({});
+const test4 = validator<{a?: number} | number>()({});
+const test5 = validator<{a?: number} | number>()({ a: 1 });
+const test6 = validator<{a?: number} | number>()(1);
+const test7 = validator<{a?: number} | [1?]>()(1);
+const test8 = validator<{a?: number} | [1?]>()([2]);
+const test9 = validator<{a?: number} | [1?]>()([1]);
+const test10 = validator<{a?: number} | [1?]>()([]);
+const test11 = validator<{a: {a: number}}>()({a: {a: 42}});
+const test12 = validator<{a: {a: number}}>()({a: {a: '42'}});
+
+// The following is only relevant for type system discussions
+// `Exactify` is an `Exact` utility proposed by the community
+// Below, we highlight its limitations in real-world scenarios
+
+type Exactify<T, X extends T> = T & {
+  [K in keyof X]: K extends keyof T ? X[K] : never
+}
+
+declare function validatorExactify<V>(): <S extends Exactify<V, S>>(select: S) => S;
+
+const testE0 = validatorExactify<{a: number}>()({ a: 2, b: 2 });
+const testE1 = validatorExactify<{a: number}>()({ a: '2', b: 2 }); // can't tell why it's wrong
+const testE2 = validatorExactify<{a: number}>()({ a: 1 });
+const testE3 = validatorExactify<{a: number}>()({});
+const testE4 = validatorExactify<{a?: number} | number>()({});
+const testE5 = validatorExactify<{a?: number} | number>()({ a: 1 }); // fails on mixed unions
+const testE6 = validatorExactify<{a?: number} | number>()(1);
+const testE7 = validatorExactify<{a?: number} | [1?]>()(1);
+const testE8 = validatorExactify<{a?: number} | [1?]>()([2]); // can't tell why it's wrong
+const testE9 = validatorExactify<{a?: number} | [1?]>()([1]); // can't tell why it's wrong
+const testE10 = validatorExactify<{a?: number} | [1?]>()([]);
+const testE11 = validatorExactify<{a: {a: number}}>()({a: {a: 42}}); // does not narrow user input
+const testE12 = validatorExactify<{a: {a: number}}>()({a: {a: '42'}}); // can't tell why it's wrong
+
 
 main();
