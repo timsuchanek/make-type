bd7e317684d61659668dc2111600576ac6deb0b8
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-01-26 14:42:14 +0100

    improve intellisense for Exact

--- Exact.ts
+++ Exact.ts
@@ -2,8 +2,10 @@
  * `Exact` forces a type to comply by another type. It will need to be a subset
  * and must have exactly the same properties, no more, no less.
  */
-type Exact<A, W> = W & {
-    [K in keyof A]: K extends keyof W ? A[K] : never
+type Exact<A, W> = {
+    [K in keyof A]: K extends keyof W
+    ? A[K] extends W[K] ? A[K] : W[K]
+    : never
 }
 
 export {Exact};
--- draft.ts
+++ draft.ts
@@ -21,36 +21,20 @@ const test0 = validateType<Prisma.UserSelect>({ id: true });
  * TS has a limitations, we cannot 'just' pass one of the generics. So we have
  * to split the generic input into virtual functions like demonstrated below:
  */
-declare function validator<V>(): <S>(select: Narrow<Exact<S, V>>) => S;
+declare function validator<V>(): <S>(select: Exact<S, V>) => S;
 
 const userValidator = validator<Prisma.UserSelect>();
-
-/**
- * Otherwise, since the .find methods also perform type checks, then why not
- * just use `Narrow` and let it do its job. This is a more elegant solution:
- */
-declare function select<S>(select: Narrow<S>): S;
 
 async function main() {
   const prisma = new PrismaClient()
 
-  // So here are the options we can have so far:
-  // - A generic validator factory function
-  // - Just a function that narrows the values
-  //   This is not something ts does by default
-  //   It's enabled by `Narrow`, see `Narrow.ts`
-  const select0 = userValidator({ id: true, name: true });  // narrowed
-  const select1 = userValidator({ id: true, xxx: true });   // and error
+  const select0 = userValidator({ id: true, name: true });
+  const select1 = userValidator({ id: true, xxx: true });
+  const select2 = userValidator({ id: 42 });
 
   const data0 = await prisma.user.findFirst({ select: select0 });
   const data1 = await prisma.user.findFirst({ select: select1 });
 
-  const select2 = select({ id: true, name: true }); // narrowed
-  const select3 = select({ id: true, xxx: true });  // no errors
-
-  const data2 = await prisma.user.findFirst({ select: select2 });
-  const data3 = await prisma.user.findFirst({ select: select3 });
-  
   const select4 = userValidator({ asd: true });  // now errors
 
   prisma.$disconnect();
