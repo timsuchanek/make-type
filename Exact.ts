06d9cbd63100ad27b9b950998a34d1d48627bafc
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-01-26 17:14:48 +0100

    cleanup

--- Exact.ts
+++ Exact.ts
@@ -2,15 +2,6 @@
  * `Exact` forces a type to comply by another type. It will need to be a subset
  * and must have exactly the same properties, no more, no less.
  */
-// type Exact<A, W> = {
-//     [K in keyof W]: K extends keyof A
-//     ? A[K] extends W[K] ? A[K] & W[K] : W[K]
-//     : W[K]
-// } & {
-//     [K in keyof A]: K extends keyof W
-//     ? A[K] extends W[K] ? A[K] & W[K] : W[K]
-//     : never
-// }
 
 /**
  * `Exact` forces a type to comply by another type. It will need to be a subset
