8b150cc370114d6d995524e97796740f0e3e7e85
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-01-27 04:51:37 +0100

    bring support for ts>=4.2

--- Exact.ts
+++ Exact.ts
@@ -1,14 +1,17 @@
 import {Cast} from "./Cast";
+import {Narrowable} from "./Narrowable";
 
 /**
  * `Exact` forces a type to comply by another type. It will need to be a subset
  * and must have exactly the same properties, no more, no less.
  */
-type Exact<A, W> =
-    W extends unknown ? A extends W ? Cast<{
+type Exact<A, W = unknown> = 
+W extends unknown ?
+    A extends Narrowable ? Cast<A, W> : {
         [K in keyof A]: K extends keyof W
         ? Exact<A[K], W[K]>
         : never
-    }, A> : W : never;
+    }
+: never;
 
 export {Exact};
