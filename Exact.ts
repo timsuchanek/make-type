236153bdf480b6a12ecc35a7f00be1e9770034ed
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2021-01-28 02:47:18 +0100

    cleanup

--- Exact.ts
+++ Exact.ts
@@ -6,16 +6,9 @@ import {Narrowable} from "./Narrowable";
  * and must have exactly the same properties, no more, no less.
  */
 type Exact<A, W = unknown> = 
-W extends unknown ?
-    A extends Narrowable ? Cast<A, W> : Cast<{
-        [K in keyof A]: K extends keyof W
-        ? Exact<A[K], W[K]>
-        : never
-    }, {
-        [K in keyof W]: K extends keyof A
-        ? Exact<A[K], W[K]>
-        : W[K]
-    }>
+W extends unknown ? A extends Narrowable ? Cast<A, W> : Cast<
+{[K in keyof A]: K extends keyof W ? Exact<A[K], W[K]> : never},
+{[K in keyof W]: K extends keyof A ? Exact<A[K], W[K]> : W[K]}>
 : never;
 
 export {Exact};
